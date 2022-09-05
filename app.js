b131b4276543ea4d02e36e283f1383dd6688cdcc
NiyaRakhewar <niharikarakhewar@gmail.com>
2022-09-05 17:32:44 +0530

    practice

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log("script is working in different file");
+
+// var username = prompt("give me username")
+
+// var welcomeMessage = "this script works !!! "+ username;
+
+// alert(welcomeMessage);
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+console.log(btnTranslate)
