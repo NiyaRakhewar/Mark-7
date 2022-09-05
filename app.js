a5af4697b7ec8893be80b942300bd29d52c7a5ab
NiyaRakhewar <niharikarakhewar@gmail.com>
2022-09-05 22:49:55 +0530

    update

--- app.js
+++ app.js
@@ -7,5 +7,42 @@
 // alert(welcomeMessage);
 
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/sith.json"
 
-console.log(btnTranslate)
+// outputDiv.innerText = "Niya"
+// console.log(txtInput)
+
+function getTranslateURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+// console.log(outputDiv)
+
+function errrorHandler(error){
+    console.log("error occured", error)
+    alert("something wrong with server !! try again after sometime")
+}
+
+function clickEventHandler(){
+    // outputDiv.innerText = "bannannanannan "+ txtInput.value;
+
+    var innerText = txtInput.value;
+
+    fetch(getTranslateURL(innerText))
+        .then( newFunction())
+        .then(json => { 
+            var translateText = json.contents.translated;
+            outputDiv.innerText = translateText;
+            })
+        .catch(errrorHandler)
+
+    function newFunction() {
+        return response => response.json();
+    }
+};
+
+btnTranslate.addEventListener("click",clickEventHandler)
+
